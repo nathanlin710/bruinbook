aa74116e71cdcf5666db42cbfe066a799d19c1ca
Nathan Lin <nathanlin710@gmail.com>
2021-05-02 14:42:12 -0700

    post test commit

---
+++ bruinbook-frontend/src/components/Post.js
@@ -0,0 +1,26 @@
+function Post() {
+    return (
+        <div class="row">
+          <div class="col">
+            <form>
+              <legend>New Post</legend>
+              <div>
+                <label>Title</label>
+                <input type="text" name="title" placeholder="Title"/>
+              </div>
+              <div>
+                <label>URL</label>
+                <input type="url" name="url" placeholder="https://www.google.com"/>
+              </div>
+              <div>
+                <label>Text</label>
+                <input type="text" name="text" placeholder="Text"/>
+              </div>
+              <div>
+                <button type="submit">Create Post</button>
+              </div>
+            </form>
+          </div>
+        </div>
+    )
+}

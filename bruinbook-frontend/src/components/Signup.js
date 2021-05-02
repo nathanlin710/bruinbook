25451fc72c89a5b5a5fae0ebd3fac0d852dc4aee
Eric Yang <eric8yang@gmail.com>
2021-05-02 14:23:00 -0700

    added compoennts folder

---
+++ bruinbook-frontend/src/components/Signup.js
@@ -0,0 +1,21 @@
+function Signup() {
+    return (
+        <form>
+            <label htmlFor="email">Username</label> <br />
+            <input
+                type="email"
+                name="email"
+            />
+            <br />
+            <label htmlFor="comment">Email</label>
+            <textarea
+                name="email"
+            />
+            <br />
+            
+            <button type="submit">Send it!</button>
+        </form>
+    )
+}
+
+export default Signup

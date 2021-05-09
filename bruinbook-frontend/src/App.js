c16f3380d09c3b6041a6a768b4e8af00cdd1458c
Nathan Lin <nathanlin710@gmail.com>
2021-05-08 21:33:22 -0700

    CreatePost components

--- bruinbook-frontend/src/App.js
+++ bruinbook-frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import CreatePost from './components/CreatePost';
 import Page from './components/Post'
 import Signup from './components/Signup'
 function App() {
   return (
     <div>
     <Signup />
+    <CreatePost />
     <Page />
     </div>
   );
